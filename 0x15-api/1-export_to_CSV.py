832bba8b3c1b871f0e2d3d813080caa5279ddba0
r0z0x <abde@gmail.com>
2024-04-23 01:58:30 +0000

    lD

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,40 +1,22 @@
 #!/usr/bin/python3
-"""ghather data from an API"""
-import csv
-import requests
-from sys import argv
-
-
-def gatherData(employeeID):
-    """Gather data from an API and save it to a CSV file"""
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employeeID}"
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{employeeID}/todos"
-
-    try:
-        response = requests.get(user_url)
-        if response.status_code == 200:
-            user = response.json()
-            response = requests.get(todo_url)
-            if response.status_code == 200:
-                todos = response.json()
-                with open('{}.csv'.format(employeeID), 'w') as csvfile:
-                    writer = csv.writer(csvfile, delimiter=',',
-                                        quotechar='"', quoting=csv.QUOTE_ALL)
-                    for todo in todos:
-                        writer.writerow([todo['userId'], user['username'],
-                                        todo['completed'], todo['title']])
-            else:
-                print("An error occured")
-        else:
-            print("An error occured")
-    except (Exception):
-        print("An error occured")
-        return 0
-
-
+"""Exports data in the CSV format"""
 if __name__ == "__main__":
-    """ Only executes as main"""
-    if len(argv) == 2 and argv[1].isdigit():
-        gatherData(argv[1])
-    else:
-        print("Usage: ./1-export_to_CSV.py <employee ID>")
+
+    import csv
+    import requests
+    import sys
+
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    name = user.json().get('username')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+    filename = userId + '.csv'
+    with open(filename, mode='w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL, lineterminator='\n')
+        for task in todos.json():
+            if task.get('userId') == int(userId):
+                writer.writerow([userId, name, str(task.get('completed')),
+                                 task.get('title')])
